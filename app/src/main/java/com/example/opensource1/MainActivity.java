6d9748ea1551bcc96be8d3f1f6bc1c10e5769295
SH <zzzsunghyun@gmail.com>
2022-05-15 22:28:23 +0900

    hihi

---
+++ app/src/main/java/com/example/opensource1/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.opensource1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText tip;
+    Button mdbtn, wtbtn, cotbtn, covidbtn;
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mdbtn = findViewById(R.id.mdButton);
+        wtbtn = findViewById(R.id.weatherButton);
+        covidbtn = findViewById(R.id.covidButton);
+        cotbtn = findViewById(R.id.cotButton);
+        // 할당
+
+        mdbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Microdust.class);
+                startActivity(intent);
+            }
+        });
+        cotbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Cotgaru.class);
+                startActivity(intent);
+            }
+        });
+        wtbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Weather.class);
+                startActivity(intent);
+            }
+        });
+        covidbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Covid.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
