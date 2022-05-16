92d089926a75ffa22467194c97cd273f7dd8f036
SH <zzzsunghyun@gmail.com>
2022-05-16 16:28:14 +0900

    버튼 누르면 tip변경

--- app/src/main/java/com/example/opensource1/MainActivity.java
+++ app/src/main/java/com/example/opensource1/MainActivity.java
@@ -8,20 +8,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    EditText tip;
-    Button mdbtn, wtbtn, cotbtn, covidbtn;
+    TextView tip;
+    Button mdbtn, wtbtn, cotbtn, covidbtn, testbtn;
+    int x=0;
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tip = findViewById(R.id.tipText);
         mdbtn = findViewById(R.id.mdButton);
         wtbtn = findViewById(R.id.weatherButton);
         covidbtn = findViewById(R.id.covidButton);
         cotbtn = findViewById(R.id.cotButton);
+        testbtn = findViewById(R.id.testButton);
+        String s1 = new String("1번팁");
+        String s2 = "2번팁";
+        String s3 = new String("3번팁");
+        String s4 = new String("4번팁");
         // 할당
 
         mdbtn.setOnClickListener(new View.OnClickListener() {
@@ -52,6 +62,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        testbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                x++;
+                if(x==1){
+                    tip.setText(s1);
+                }
+                else if(x==2)
+                {
+                    tip.setText(s2);
+                }
+                else if(x==3)
+                {
+                    tip.setText(s3);
+                }
+                else
+                {
+                    tip.setText(s4);
+                    x=0;
+                }
+
+            }
+        });//함수 구현 부분
 
     }
 }
